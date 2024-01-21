31d9ae8c4776e33902f5ebf12ad72dbf94c5b89a
梁普榉 <34727387+smallmonsters@users.noreply.github.com>
2024-01-21 15:57:57 +0800

    recyclerviewcase 基础了解完

--- recyclerviewcase/src/main/java/com/example/recyclerviewcase/MainActivity.java
+++ recyclerviewcase/src/main/java/com/example/recyclerviewcase/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.recyclerviewcase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+  RecyclerView recyclerView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.recyclerview_itme);
+    setContentView(R.layout.activity_main);
+    recyclerView = findViewById(R.id.recyclerId);
+    ArrayList<UserModel> userList = new ArrayList<UserModel>();
+    for (int i = 0; i < 10; i++) {
+      userList.add(new UserModel("User" + i));
+    }
+    RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this, userList);
+    recyclerView.setAdapter(recyclerViewAdapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
   }
 }
--- recyclerviewcase/src/main/java/com/example/recyclerviewcase/RecyclerViewAdapter.java
+++ recyclerviewcase/src/main/java/com/example/recyclerviewcase/RecyclerViewAdapter.java
@@ -1,33 +1,52 @@
 package com.example.recyclerviewcase;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+  private Context context;
+  private ArrayList<UserModel> userList;
+
+  //构造器
+  public RecyclerViewAdapter(Context context, ArrayList<UserModel> userList) {
+    this.context = context;
+    this.userList = userList;
+
+  }
 
   @NonNull
   @Override
   public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
+    View view = LayoutInflater.from(this.context).inflate(R.layout.recyclerview_itme, parent, false);
+    return new MyViewHolder(view);
   }
 
   @Override
   public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
-
+    holder.textView.setText(userList.get(position).getUser());
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return this.userList.size();
   }
 
   public class MyViewHolder extends RecyclerView.ViewHolder {
 
+    TextView textView;
+
     public MyViewHolder(@NonNull View itemView) {
       super(itemView);
+      this.textView = itemView.findViewById(R.id.username);
     }
   }
 }
